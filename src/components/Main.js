e72e1db2c36399d296e68ea3948526ff618fe9a3
vabarnabas <vabarnabas@gmail.com>
2022-01-05 14:45:36 +0100

    📄 Small Fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ const Main = ({ responseData }) => {
 
     return (
         <div className='w-full h-full grid grid-rows-2 text-slate-600'>
-            <div className="flex flex-col items-center justify-center print:hidden">
+            <div className="flex flex-col items-center justify-center">
                 <div className="mb-6 flex flex-col items-center justify-center">
                     <p className="text-6xl font-semibold">{responseData.at(-2).point}</p>
                     <p className="text-xs font-semibold">PONTSZÁM</p>
@@ -19,7 +19,7 @@ const Main = ({ responseData }) => {
                     <p className="text-xs font-semibold">SZAVAZAT</p>
                 </div>
             </div>
-            <div className="mx-4 mb-8 flex flex-col items-center justify-start overflow-y-scroll scrollbar-hide space-y-2 print:row-span-2">
+            <div className="mx-4 mb-8 flex flex-col items-center justify-start overflow-y-scroll scrollbar-hide space-y-2">
             {responseData.slice(0, responseData.length-3).map((item) => (
                 <div key={uuidv4()} className="flex items-center justify-center py-2 px-6 bg-slate-200 rounded-md w-full md:w-[40%]">
                     <p className="mr-auto w-2/3 font-semibold">{item.event}</p>
